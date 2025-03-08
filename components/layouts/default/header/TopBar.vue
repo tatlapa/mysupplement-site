93fe9537cba752c7f0c8aa102f71659e60aaa834
Richard Jérémy <jeremy.rc77@gmail.com>
2025-03-08 18:52:10 +0700

    s

--- components/layouts/default/header/TopBar.vue
+++ components/layouts/default/header/TopBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ChevronDown, House, Sun, Moon, Search, User } from "lucide-vue-next";
+import { ChevronDown, House, Sun, Moon, Search, User, ShoppingCart } from "lucide-vue-next";
 
 const authStore = useAuthStore();
 const isDark = ref(false);
@@ -41,7 +41,7 @@ const switchToLogin = () => {
 <template>
   <div class="bg-muted-foreground/[0.03] shadow-md border-b">
     <div class="container mx-auto py-1 xl:py-2 px-4 sm:px-6 lg:px-8">
-      <div class="flex flex-wrap justify-between items-center">
+      <div class="flex justify-between items-center">
         <NuxtLink
           to="/"
           class="rounded-full border p-2 hover:bg-secondary hover:duration-75"
@@ -59,56 +59,58 @@ const switchToLogin = () => {
           />
         </div>
 
-        <div v-if="!authStore.isAuthenticated" class="flex space-x-2">
-          <UtilsRegisterForm
-            v-model:open="showRegisterDialog"
-            @switchToLogin="switchToLogin"
-          />
-          <UtilsLoginForm
-            v-model:open="showLoginDialog"
-            @switchToRegister="switchToRegister"
-          />
-        </div>
+          <div class="flex">
+            <div v-if="!authStore.isAuthenticated" class="flex gap-2">
+              <UtilsRegisterForm
+                v-model:open="showRegisterDialog"
+                @switchToLogin="switchToLogin"
+              />
+              <UtilsLoginForm
+                v-model:open="showLoginDialog"
+                @switchToRegister="switchToRegister"
+              />
+            </div>
 
-        <div class="flex gap-2 items-center">
-          <DropdownMenu v-if="authStore.isAuthenticated">
-            <DropdownMenuTrigger as-child>
-              <Button variant="outline" size="sm" class="gap-x-2">
-                                <component :is="User" class="w-5 h-5" />
-
-                {{ authStore.user?.name }}
-                <ChevronDown
-                  class="w-4 h-4 transition-transform duration-200"
-                />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent class="w-fit" align="end">
-              <DropdownMenuLabel class="font-normal">
-                <div class="flex flex-col space-y-1">
-                  <p class="text-sm font-medium leading-none">My account</p>
-                  <p class="text-xs leading-none text-muted-foreground">
-                    {{ authStore.user?.email }}
-                  </p>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <NuxtLink to="/profile">
-                    <span>My profile</span>
-                  </NuxtLink>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem @click="authStore.logout">
-                <span>Disconnect</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <Button variant="ghost" @click="toggleTheme">
-            <Sun v-if="!isDark" class="size-6" />
-            <Moon v-else class="size-6" />
-          </Button variant="ghost">
+            <div class="flex gap-2 items-center">
+              <DropdownMenu v-if="authStore.isAuthenticated">
+                <DropdownMenuTrigger as-child>
+                  <Button variant="outline" size="sm" class="gap-2">
+                    <component :is="User" class="w-5 h-5" />
+                    {{ authStore.user?.name }}
+                    <ChevronDown
+                      class="w-4 h-4 transition-transform duration-200"
+                    />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent class="w-fit" align="end">
+                  <DropdownMenuLabel class="font-normal">
+                    <div class="flex flex-col space-y-1">
+                      <p class="text-sm font-medium leading-none">My account</p>
+                      <p class="text-xs leading-none text-muted-foreground">
+                        {{ authStore.user?.email }}
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <NuxtLink to="/profile">
+                        <span>My profile</span>
+                      </NuxtLink>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem @click="authStore.logout">
+                    <span>Disconnect</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Button variant="ghost" @click="toggleTheme">
+                <Sun v-if="!isDark" class="size-6" />
+                <Moon v-else class="size-6" />
+              </Button variant="ghost">
+              <NuxtLink class="flex gap-1"><ShoppingCart></ShoppingCart>Cart</NuxtLink>
+            </div>
         </div>
       </div>
     </div>
