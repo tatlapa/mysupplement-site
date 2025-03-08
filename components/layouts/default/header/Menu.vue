16a56bd4c8235682dc8a9f9676c1a2a6b0fbab31
Richard Jérémy <jeremy.rc77@gmail.com>
2025-03-08 12:14:45 +0700

    s

--- components/layouts/default/header/Menu.vue
+++ components/layouts/default/header/Menu.vue
@@ -8,18 +8,37 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+
+const menuItems = [
+  {
+    title: "Shop",
+    path: "/shop",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
 </script>
 
 <template>
-  <NavigationMenu>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <NavigationMenuLink>Link</NavigationMenuLink>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
+  <div class="flex flex-row-reverse mt-2">
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem v-for="item in menuItems" :key="item.path">
+          <NuxtLink :to="item.path">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()">{{
+              item.title
+            }}</NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </div>
 </template>
