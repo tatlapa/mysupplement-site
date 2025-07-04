280cf32763e6ca47e76b69ec323feec90651d4fe
Richard Jérémy <jeremy.rc77@gmail.com>
2025-07-04 17:08:31 +0700

    Update CardItem.vue

--- components/views/shop/CardItem.vue
+++ components/views/shop/CardItem.vue
@@ -18,24 +18,28 @@ const selectedProduct = computed(() => product.value);
 
 <template>
   <NuxtLink :to="`/shop/products/${selectedProduct.id}`">
-    <Card class="shadow-md transition-transform hover:scale-105">
-      <CardHeader>
-        <CardTitle class="text-xl">{{ product.name }}</CardTitle>
+    <Card
+      class="shadow-md transition-transform hover:scale-105 h-full flex flex-col"
+    >
+      <CardHeader class="flex-shrink-0">
+        <CardTitle class="text-xl line-clamp-2">{{ product.name }}</CardTitle>
         <CardDescription class="text-sm text-gray-500">
           {{ product.category?.name || "No Category" }}
         </CardDescription>
       </CardHeader>
 
-      <CardContent>
+      <CardContent class="flex-1 flex flex-col">
         <img
           :src="`${config.public.backUrl}${product.image_url}`"
           :alt="product.name"
-          class="w-full h-48 object-cover rounded-md"
+          class="w-full h-56 object-cover object-center rounded-md flex-shrink-0"
         />
-        <p class="text-lg font-medium text-gray-800">{{ product.price }} $</p>
+        <p class="text-lg font-medium text-gray-800 mt-4">
+          {{ product.price }} $
+        </p>
       </CardContent>
 
-      <CardFooter class="flex flex-start gap-3">
+      <CardFooter class="flex flex-start gap-3 flex-shrink-0">
         <p>Stock : {{ product.stock_quantity }}</p>
       </CardFooter>
     </Card>
